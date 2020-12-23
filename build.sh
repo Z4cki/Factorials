9a5031cfed984ca90137d6cd832f4295474a2f14
Z4cki <legocrack@freenet.de>
2020-12-23 16:16:08 +0100

    removed one line

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mkdir -p bin
-g++ src/factorial.cpp -o bin/factorial -pthread -lgmp -lgmpxx -std=c++17
+mkdir -p bin && g++ src/factorial.cpp -o bin/factorial -pthread -lgmp -lgmpxx -std=c++17
