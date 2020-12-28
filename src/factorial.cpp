f519b5746442cbfab2dfab3c7452fb352c0043c0
Z4cki <legocrack@freenet.de>
2020-12-28 23:22:48 +0100

    improved thread management

--- src/factorial.cpp
+++ src/factorial.cpp
@@ -8,7 +8,7 @@
 #ifndef THREADS_COUNT_DEFINED
 #define THREADS_COUNT_DEFINED
 int iKerne = std::thread::hardware_concurrency();
-int threads = 512; //1024; //2048; //4096; // 8192; //16128;
+int threads = std::thread::hardware_concurrency(); //512; //1024; //2048; //4096; // 8192; //16128;
 #endif
 
 void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end);
@@ -19,20 +19,20 @@ int main()
 {
 	int number = 1000000;
 	Factorial(number);
-	
 }
 
 void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end)
 {
-    debug::db_timer timer;
+    //debug::db_timer timer;
 	mpz_class a = 1;
 	for (int i = start; i <= end; i += threads) a *= i;
 	mpzPromise.set_value(a);
-    timer.end("Finished split thread");
+    //timer.end("Finished split thread");
 }
 
 void Factorial(int number)
 {
+    debug::db_timer timer;
 	switch (number)
 	{
 		case 100000: 
@@ -70,19 +70,29 @@ void Factorial(int number)
 	{
 		factFuture[i] = factPromise[i].get_future();
 		factThread[i] = std::thread(CalculateFactorial, std::move(factPromise[i]), i + 1, number);
+        factThread[i].detach();
 	}
 	
-	int j = 0;
-	for (int i = threads; i < threads * 2 - 1 ; i++)
+    int j = 0;                      
+	for (int i = threads; i < threads * 2 - 1; i++)
 	{
 		factFuture[i] = factPromise[i].get_future();
 	    factThread[i] = std::thread(MergeResult, std::move(factPromise[i]), factFuture[j].get(), factFuture[j+1].get());
 		j+=2;
+        factThread[i].detach();
  	}
 
-    gmp_printf("%Zd", factFuture[threads * 2 - 2]);
+    //factFuture[threads * 2 - 2] = factPromise[threads * 2 - 2].get_future();
+	//factThread[threads * 2 - 2] = std::thread(MergeResult, std::move(factPromise[threads * 2 - 2]), factFuture[j].get(), factFuture[j+1].get());
+
+    mpz_class result = factFuture[threads * 2 - 2].get();
+    timer.end();
+    //factThread[threads * 2 - 2].detach();
+
+    //gmp_printf("%Zd\n", result);
 	
-	for (auto &t : factThread) t.join();
+    //for (int i = threads; i < threads * 2 - 1; i++) factThread[i].join();
+	//for (auto &t : factThread) t.join();
 }
 
 void MergeResult(std::promise<mpz_class>&& mpzPromise, mpz_class a, mpz_class b) 
