5f0dd8e3141a53466e188228ca846fe5bec5d3e8
Z4cki <legocrack@freenet.de>
2020-12-29 00:24:06 +0100

    rename arrays, function tweaks, remove global var

--- src/factorial.cpp
+++ src/factorial.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <future>
 #include <thread>
 
@@ -8,20 +8,22 @@
 #ifndef THREADS_COUNT_DEFINED
 #define THREADS_COUNT_DEFINED
 int iKerne = std::thread::hardware_concurrency();
-int threads = std::thread::hardware_concurrency(); //512; //1024; //2048; //4096; // 8192; //16128;
 #endif
 
-void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end);
+void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end, int threads);
 void Factorial(int number);
 void MergeResult(std::promise<mpz_class>&& mpzPromise, mpz_class a, mpz_class b);
 
 int main()
 {
-	int number = 1000000;
+	int number;
+	std::cout << "Number: ";
+	std::cin >> number;
 	Factorial(number);
+	return 0;
 }
 
-void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end)
+void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end, int threads)
 {
     //debug::db_timer timer;
 	mpz_class a = 1;
@@ -32,7 +34,13 @@ void CalculateFactorial(std::promise<mpz_class>&& mpzPromise, int start, int end
 
 void Factorial(int number)
 {
+	if (number <= 1) 
+	{
+		std::cout << "Is that input worth to start ANY threads?\n";
+		 return;
+	}
     debug::db_timer timer;
+	int threads; //512; //1024; //2048; //4096; // 8192; //16128;
 	switch (number)
 	{
 		case 100000: 
@@ -47,41 +55,43 @@ void Factorial(int number)
 		}
 		case 10000000: 
 		{
-			threads = 4096;
+			threads = 8192;
 			break;
 		}
 		case 100000000: 
 		{
-			threads = 8192;
+			threads = 64000;
 			break;
 		}
 		default: 
 		{
-			threads = 16000;
+			if (number < 100000) threads = std::thread::hardware_concurrency();
+			else threads = 32000;
+			if (threads > number) threads = number;
 			break;
 		}
 	}
 
-	std::promise<mpz_class> factPromise[threads * 2 - 1];
-	std::future<mpz_class>  factFuture[threads * 2 - 1];
+	std::promise<mpz_class> promisePool[threads * 2 - 1];
+	std::future<mpz_class>  futurePool[threads * 2 - 1];
 		
-	std::thread factThread[threads * 2];
+	std::thread threadPool[threads * 2];
 	for (int i = 0; i < threads; i++)
 	{
-		factFuture[i] = factPromise[i].get_future();
-		factThread[i] = std::thread(CalculateFactorial, std::move(factPromise[i]), i + 1, number);
-        factThread[i].detach();
+		futurePool[i] = promisePool[i].get_future();
+		threadPool[i] = std::thread(CalculateFactorial, std::move(promisePool[i]), i + 1, number, threads);
+        threadPool[i].detach();
 	}
 	
     int j = 0;                      
 	for (int i = threads; i < threads * 2 - 1; i++)
 	{
-		factFuture[i] = factPromise[i].get_future();
-	    factThread[i] = std::thread(MergeResult, std::move(factPromise[i]), factFuture[j].get(), factFuture[j+1].get());
+		futurePool[i] = promisePool[i].get_future();
+	    threadPool[i] = std::thread(MergeResult, std::move(promisePool[i]), futurePool[j].get(), futurePool[j+1].get());
 		j+=2;
-        factThread[i].detach();
+        threadPool[i].detach();
  	}
-    mpz_class result = factFuture[threads * 2 - 2].get();
+    mpz_class result = futurePool[threads * 2 - 2].get();
     timer.end();
 
     //gmp_printf("%Zd\n", result);
